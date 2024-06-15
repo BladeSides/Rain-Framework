76e3e4eed5ea2065c5b86bed1354f84ca191e814
Pyush Chamoli <43913393+BladeSides@users.noreply.github.com>
2024-06-15 18:24:46 +0200

    Add Smooth Lerp

--- Assets/RainFramework/Scripts/Math/MathFunctions.cs
+++ Assets/RainFramework/Scripts/Math/MathFunctions.cs
@@ -29,6 +29,54 @@ namespace RainFramework.Math
         {
             return Mathf.Atan2(direction.y,direction.x);
         }
+
+
+        // Functions for Smooth Lerping Below
+        public static float LerpSmooth(float a, float b, float dt, float h)
+        {
+            return b + (a - b) * Mathf.Pow(2, -dt / h);
+        }
+
+        public static float LerpSmooth(float a, float b, float dt, float t, float precision)
+        {
+            return b + (a - b) * Mathf.Pow(2, -dt / GetHalfLife(t, precision));
+        }
+
+        public static Vector3 LerpSmooth(Vector3 a, Vector3 b, float dt, float h)
+        {
+            return b + (a - b) * Mathf.Pow(2, -dt / h);
+        }
+
+        public static Vector3 LerpSmooth(Vector3 a, Vector3 b, float dt, float t, float precision)
+        {
+            return b + (a - b) * Mathf.Pow(2, -dt / GetHalfLife(t, precision));
+        }
+
+        public static Quaternion SlerpSmooth(Quaternion a, Quaternion b, float dt, float h)
+        {
+            return (Quaternion.Slerp(a, b, 1 - Mathf.Pow(2, -dt / h)));
+        }
+
+        public static Quaternion SlerpSmooth(Quaternion a, Quaternion b, float dt, float t, float precision)
+        {
+            return (Quaternion.Slerp(a, b, 1 - Mathf.Pow(2, -dt / GetHalfLife(t, precision))));
+        }
+
+        public static Quaternion LerpSmooth(Quaternion a, Quaternion b, float dt, float h)
+        {
+            return (Quaternion.Lerp(a, b, 1 - Mathf.Pow(2, -dt / h)));
+        }
+
+        public static Quaternion LerpSmooth(Quaternion a, Quaternion b, float dt, float t, float precision)
+        {
+            return (Quaternion.Lerp(a, b, 1 - Mathf.Pow(2, -dt / GetHalfLife(t, precision))));
+        }
+
+        public static float GetHalfLife(float t, float precision)
+        {
+            return -t / Mathf.Log(precision, 2);
+        }
+
     }
 
 }
